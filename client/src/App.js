dedc999bedeb020eae66263e9b6d2c293161351c
Eddy Josafat Hernandez Vega <ejosafat@hope.local>
2018-12-07 19:52:15 +0000

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
           <div className="App">
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
+              <h1>Esto se ha cambiado</h1>
               <Link to="/">Home</Link>
               <Link to="/otherpage">Other page</Link>
             </header>
